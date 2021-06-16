995a8bfea51c6413774b9054581a57b7dbb253ed
Michelle Berta <bertafamily@sbcglobal.net>
2021-06-16 15:17:53 -0500

    still trying to find error server.js and db.js

--- public/db.js
+++ public/db.js
@@ -2,12 +2,11 @@ let db;
 
 let budget;
 
-
 // Create a new db request for a "budget" database.
 const request = window.indexedDB.open(budget, 1);
 
 request.onupgradeneeded = function (e) {
-  console.log('Upgrade needed in IndexDB');
+  console.log("Upgrade needed in IndexDB");
 
   const { oldVersion } = e;
   const newVersion = e.newVersion || db.version;
@@ -17,7 +16,7 @@ request.onupgradeneeded = function (e) {
   db = e.target.result;
 
   if (db.objectStoreNames.length === 0) {
-    db.createObjectStore('budgetStore', { autoIncrement: true });
+    db.createObjectStore("budgetStore", { autoIncrement: true });
   }
 };
 
@@ -26,13 +25,13 @@ request.onerror = function (e) {
 };
 
 function checkDatabase() {
-  console.log('check db invoked');
+  console.log("check db invoked");
 
   // Open a transaction on your BudgetStore db
-  let transaction = db.transaction(['budgetStore'], 'readwrite');
+  let transaction = db.transaction(["budgetStore"], "readwrite");
 
   // access your BudgetStore object
-  const store = transaction.objectStore('budgetStore');
+  const store = transaction.objectStore("budgetStore");
 
   // Get all records from store and set to a variable
   const getAll = store.getAll();
@@ -41,12 +40,12 @@ function checkDatabase() {
   getAll.onsuccess = function () {
     // If there are items in the store, we need to bulk add them when we are back online
     if (getAll.result.length > 0) {
-      fetch('/api/transaction/bulk', {
-        method: 'POST',
+      fetch("/api/transaction/bulk", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
         },
       })
         .then((response) => response.json())
@@ -54,14 +53,14 @@ function checkDatabase() {
           // If our returned response is not empty
           if (res.length !== 0) {
             // Open another transaction to BudgetStore with the ability to read and write
-            transaction = db.transaction(['budgetStore'], 'readwrite');
+            transaction = db.transaction(["budgetStore"], "readwrite");
 
             // Assign the current store to a variable
-            const currentStore = transaction.objectStore('budgetStore');
+            const currentStore = transaction.objectStore("budgetStore");
 
             // Clear existing entries because our bulk add was successful
             currentStore.clear();
-            console.log('Clearing store 🧹');
+            console.log("Clearing store 🧹");
           }
         });
     }
@@ -69,27 +68,27 @@ function checkDatabase() {
 }
 
 request.onsuccess = function (e) {
-  console.log('success');
+  console.log("success");
   db = e.target.result;
 
   // Check if app is online before reading from db
   if (navigator.onLine) {
-    console.log('Backend online! 🗄️');
+    console.log("Backend online! 🗄️");
     checkDatabase();
   }
 };
 
 const saveRecord = (record) => {
-  console.log('Save record invoked');
+  console.log("Save record invoked");
   // Create a transaction on the BudgetStore db with readwrite access
-  const transaction = db.transaction(['budgetStore'], 'readwrite');
+  const transaction = db.transaction(["budgetStore"], "readwrite");
 
   // Access your BudgetStore object store
-  const store = transaction.objectStore('budgetStore');
+  const store = transaction.objectStore("budgetStore");
 
   // Add record to your store with add method.
   store.add(record);
 };
 
 // Listen for app coming back online
-window.addEventListener('online', checkDatabase);
+window.addEventListener("online", checkDatabase);
